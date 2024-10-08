a267fd69f4f55c71b46ab9cb0d110cfe8aaa1ba9
Muhammad Fazeel Raza <muhammadfazeel15it@gmail.com>
2024-10-08 20:10:45 +0500

    JavaScript Variable2

---
+++ app.js
@@ -0,0 +1,8 @@
+var age= "I am 15 years old";
+alert(age);
+
+var visited= "You have visited this site 15 times";
+alert(visited);
+
+document.write("My birth year is 1990 <br> Data type of my declared variable in number <br><br>")
+document.write("ABC ordered 5 T-shirt(s) on XYZ Clothing store")
